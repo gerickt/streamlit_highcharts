b2a557459fbe5aa53bf22bfc6ee11646096568ee
Gerick Toro <tu_correo_electronico@example.com>
2024-12-20 15:45:46 -0400

    Refactor HTML and JavaScript for Highcharts integration
    - Changed script tags in index.html to use 'defer' for improved loading performance.
    - Added a new function in main.js to wait for Highcharts to load before rendering the chart, enhancing reliability during initialization.

--- src/streamlit_highcharts/frontend/main.js
+++ src/streamlit_highcharts/frontend/main.js
@@ -3,14 +3,31 @@ function sendValue(value) {
 }
 
 
+function waitForHighcharts(callback, maxAttempts = 10) {
+  let attempts = 0;
+  
+  const checkHighcharts = () => {
+    if (typeof Highcharts !== 'undefined') {
+      callback();
+    } else if (attempts < maxAttempts) {
+      attempts++;
+      setTimeout(checkHighcharts, 100);
+    } else {
+      console.error('No se pudo cargar Highcharts después de varios intentos');
+    }
+  };
+  
+  checkHighcharts();
+}
+
 function onRender(event) {
-  if (true) {
+  waitForHighcharts(() => {
     const {options,height} = event.detail.args
     Streamlit.setFrameHeight(height+20)
     document.getElementById("container").style.height=height+"px"
     Highcharts.chart('container', options);
     window.rendered = true
-  }
+  });
 }
 
 Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
